d5eb8b96b999a5698c6d043cf8bdc879fd31a963
root <root@ip-10-10-1-101.ec2.internal>
2016-07-05 01:05:48 +0000

    added test

--- sample_vars.sh
+++ sample_vars.sh
@@ -5,6 +5,8 @@ export AWS_AMI_ID=ami-6869aa05
 export INSTANCE_TYPE=m4.xlarge
 export AWS_VPC_ID=vpc-406ce927
 export AWS_SUBNET_ID=subnet-10556e2d
+export AWS_DB_AMI_ID=ami-2051294a
+export AWS_FMW_AMI_ID=ami-2051294a
 
 export DEFAULT_RHEL_AMI=ami-2051294a
 export DEFAULT_CENTOS_AMI=ami-6d1c2007
